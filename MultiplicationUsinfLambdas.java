2dba6e285d386ede877f8cb341425098fe65dc70
vijay <vijaypopuri2021@gmail.com>
2024-09-16 22:54:56 +0530

    mulChngds

---
+++ MultiplicationUsinfLambdas.java
@@ -0,0 +1,24 @@
+package LambdaExpressions;
+
+import java.util.Scanner;
+
+interface Multiplication{
+	double mul(int a,double b);
+}
+public class MultiplicationUsinfLambdas
+{
+
+	public static void main(String[] args) 
+	{
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Integer value");
+		int a=sc.nextInt();
+		System.out.println("Enter Double value");
+		double b=sc.nextDouble();
+		Multiplication ml=(a1,b1)->(a1*b1);
+		double res=ml.mul(a, b);
+		System.out.println(res);
+	}
+
+}
