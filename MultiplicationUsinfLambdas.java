3030ee86f46ff7228551bb3ac5caa2c840399739
vijay <vijaypopuri2021@gmail.com>
2024-09-16 22:56:15 +0530

    format chngs

--- MultiplicationUsinfLambdas.java
+++ MultiplicationUsinfLambdas.java
@@ -2,22 +2,21 @@ package LambdaExpressions;
 
 import java.util.Scanner;
 
-interface Multiplication{
-	double mul(int a,double b);
+interface Multiplication {
+	double mul(int a, double b);
 }
-public class MultiplicationUsinfLambdas
-{
 
-	public static void main(String[] args) 
-	{
+public class MultiplicationUsinfLambdas {
 
-		Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Integer value");
-		int a=sc.nextInt();
+		int a = sc.nextInt();
 		System.out.println("Enter Double value");
-		double b=sc.nextDouble();
-		Multiplication ml=(a1,b1)->(a1*b1);
-		double res=ml.mul(a, b);
+		double b = sc.nextDouble();
+		Multiplication ml = (a1, b1) -> (a1 * b1);
+		double res = ml.mul(a, b);
 		System.out.println(res);
 	}
 
